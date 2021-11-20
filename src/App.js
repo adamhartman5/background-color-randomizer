53ef043df30f0012d892f6654966b62f0b7a5c1f
Adam Hartman <adamhartman5@gmail.com>
2021-11-20 17:03:58 -0500

    Build app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
 
 function App() {
+
+  const [backgroundColor, setBackgroundColor] = useState("#000000")
+  const [randomNum, setRandomNum] = useState(0)
+
+  const backgroundColors = ["#4978c0", "#74cfe9", "#f55919", "#800000", "#008080"]
+
+  const changeBackground = () => {
+    getRandomNum();
+    setBackgroundColor(backgroundColors[randomNum]);
+  }
+
+  const getRandomNum = () => {
+    let getRandomNum = Math.floor((Math.random() * 5));
+    setRandomNum(getRandomNum);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main" style={{backgroundColor: backgroundColor}}>
+        <div className="bg-color">
+          <p className="bg-text">Background Color: {backgroundColor}</p>
+        </div>
+        <button className="button"onClick={changeBackground}>
+          Click me
+        </button>
+      </div>
     </div>
   );
 }
